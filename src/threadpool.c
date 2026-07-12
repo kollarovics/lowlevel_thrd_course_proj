82e2097b5477eab1d3ac61097b7a6d5a85081f1c
adrian <kollarovics@gmail.com>
2026-07-12 16:41:16 +0200

    Test: Running Threads

--- src/threadpool.c
+++ src/threadpool.c
@@ -36,7 +36,16 @@ void threadpool_destroy(threadpool_t* pool)
 
 void threadpool_add_task(threadpool_t* pool, void (*function)(void*), void* arg)
 {
+  task_t task;
+  task.fn = function;
+  task.arg = arg;
 
+  pthread_mutex_lock(&(pool->lock));
+  pool->task_queue[pool->queue_back] = task;
+  pool->queue_back = (pool->queue_back + 1) % QUEUE_SIZE;
+  pool->queued++;
+  pthread_mutex_unlock(&(pool->lock));
+  pthread_cond_signal(&(pool->notify));
 }
 
 void example_task(void* arg)
