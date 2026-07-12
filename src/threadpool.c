efe31e7b67f8f4318d543fa2981662fcb9cd5220
adrian <kollarovics@gmail.com>
2026-07-12 18:20:53 +0200

    Step 2: Thread Shutdown

--- src/threadpool.c
+++ src/threadpool.c
@@ -49,8 +49,10 @@ void threadpool_init(threadpool_t* pool)
 
 void threadpool_destroy(threadpool_t* pool)
 {
+    pthread_mutex_lock(&(pool->lock));
     pool->stop = 1;
     pthread_cond_broadcast(&(pool->notify));
+    pthread_mutex_unlock(&(pool->lock));
     for (int i = 0; i < THREADS; i++)
     {
         pthread_join(pool->threads[i], NULL);
