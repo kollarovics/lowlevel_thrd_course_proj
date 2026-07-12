283911eaa4c91ecef80ced18b0884a88fa32eee5
adrian <kollarovics@gmail.com>
2026-07-12 16:31:16 +0200

    Test: Running Threads

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#import <pthread.h>
+#import <stdlib.h>
+#import <stdio.h>
+#import <unistd.h>
+#import "threadpool.h"
+
+
+int main(void)
+{
+
+}
+
+
+
---
+++ src/threadpool.c
@@ -0,0 +1,30 @@
+#import <pthread.h>
+#import "threadpool.h"
+
+
+void *thread_function(void* arg)
+{
+    threadpool_t* the_pool = (threadpool_t*)arg;
+    do
+    {
+        pthread_cond_wait(&the_pool->notify, &the_pool->lock);
+    } while (the_pool->stop == 0);
+    return NULL;
+}
+
+
+void threadpool_init(threadpool_t* pool)
+{
+    pool->queued = 0;
+    pool->queue_front = 0;
+    pool->queue_back = 0;
+    pool->stop = 0;
+
+    pthread_mutex_init(&(pool->lock), NULL);
+    pthread_cond_init(&(pool->notify), NULL);
+
+    for (int i = 0; i < THREADS; i++)
+    {
+        pthread_create(&(pool->threads[i]), NULL, thread_function, pool);
+    }
+}
